d3196f35755a1ad52f9b90ae17db086d9b4b22d6
woong <workplayhard1@naver.com>
2023-07-12 01:28:17 +0900

    feat: textField Appearance 설정

---
+++ MVC/MVC/MVC/Utils/Extension/UIColor+Ext.swift
@@ -0,0 +1,22 @@
+//
+//  UIColor+Ext.swift
+//  MVC
+//
+//  Created by 서현웅 on 2023/07/12.
+//
+
+import UIKit
+
+extension UIColor {
+    static var empty: UIColor {
+        return .systemGray
+    }
+    
+    static var valid: UIColor {
+        return .systemGreen
+    }
+    
+    static var invalid: UIColor {
+        return .systemRed
+    }
+}
--- MVC/MVC/MVC/ViewController.swift
+++ MVC/MVC/MVC/ViewController.swift
@@ -17,10 +17,32 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupDefaultButtonAppearance()
+        setupSubviews()
     }
     
     private func setupDefaultButtonAppearance() {
         signInButton.setBackgroundColor(state: .disabled)
+    }
+    
+    private func setupSubviews() {
+        setupTextFieldAppearance([idTextField, pwTextField, confirmPwTextField])
+        setupTextFieldSecureContentType([pwTextField, confirmPwTextField])
+    }
+    
+    private func setupTextFieldAppearance(_ textFields: [UITextField]) {
+        let defaultBorderWidth = 2.0
+        
+        textFields.forEach {
+            $0.layer.borderColor = UIColor.empty.cgColor
+            $0.layer.borderWidth = defaultBorderWidth
+        }
+    }
+    
+    private func setupTextFieldSecureContentType(_ textFields: [UITextField]) {
+        textFields.forEach {
+            $0.textContentType = .oneTimeCode
+            $0.isSecureTextEntry = true
+        }
     }
 
     @IBAction func signInAction(_ sender: UIButton) {

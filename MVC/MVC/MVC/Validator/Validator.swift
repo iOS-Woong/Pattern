6ebb46dd1c2dce3d26070c7a5b7035f20da0f2d3
woong <workplayhard1@naver.com>
2023-07-12 02:06:33 +0900

    feat: id pw validation check

--- MVC/MVC/MVC/Validator/Validator.swift
+++ MVC/MVC/MVC/Validator/Validator.swift
@@ -15,22 +15,45 @@ protocol Validator {
     func validate(text: String?, _ validateObject: ValidationType) -> Bool
 }
 
+// external
 final class SignInValidator: Validator {
-    
     func validate(text: String?, _ type: ValidationType) -> Bool {
         guard let text else { return false }
         
         switch type {
         case .id:
-            print("idValid")
+            return checkIdValidation(text)
         case .password:
-            print("passwordValid")
+            return checkPWValidation(text)
         case .confirm:
             print("confirm Valid")
         }
         
         return true
     }
+}
+
+// internal
+extension SignInValidator {
+    private func checkIdValidation(_ text: String) -> Bool {
+        if text.count > 5 {
+            return true
+        }
+        return false
+    }
     
+    private func checkPWValidation(_ text: String) -> Bool {
+        if text.count > 7 {
+            return true
+        }
+        return false
+    }
+    
+    private func checkConfirmPWValidation(_ text: String) -> Bool {
+        
+        
+        
+        return false
+    }
     
 }

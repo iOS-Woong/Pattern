ab03dd717d5ba5d947eec2f5e5c1fd7a211ef5fa
woong <workplayhard1@naver.com>
2023-07-12 04:54:26 +0900

    feat: binding

--- MVVM/MVVM/View/ViewModel.swift
+++ MVVM/MVVM/View/ViewModel.swift
@@ -10,9 +10,54 @@ import Foundation
 class ViewModel {
     private let validator: Validator
     
+    private var id: ValidationState = .empty {
+        didSet {
+            idHandler?(id)
+        }
+    }
+    private var password: ValidationState = .empty {
+        didSet {
+            passwordHandler?(password)
+        }
+    }
+    private var confirm: ValidationState = .empty {
+        didSet {
+            confirmPasswordHandler?(confirm)
+        }
+    }
+    private var validateAll: Bool = false {
+        didSet {
+            validateAllHandler?(validateAll)
+        }
+    }
     
+    private var idHandler: ((ValidationState) -> Void)?
+    private var passwordHandler: ((ValidationState) -> Void)?
+    private var confirmPasswordHandler: ((ValidationState) -> Void)?
+    private var validateAllHandler: ((Bool) -> Void)?
     
     init(validator: Validator = SignInValidator()) {
         self.validator = validator
     }
+    
 }
+
+// bind
+extension ViewModel {
+    func bindID(handler: @escaping ((ValidationState) -> Void)) {
+        self.idHandler = handler
+    }
+    
+    func bindPassword(handler: @escaping ((ValidationState) -> Void)) {
+        self.passwordHandler = handler
+    }
+    
+    func bindConfirm(handler: @escaping ((ValidationState) -> Void)) {
+        self.confirmPasswordHandler = handler
+    }
+    
+    func bindValidateAll(handler: @escaping ((Bool) -> Void)) {
+        self.validateAllHandler = handler
+    }
+}
+

ae850c419600af5ddb9b05ee044b550f8acdf4c6
woong <workplayhard1@naver.com>
2023-07-12 03:31:57 +0900

    feat: ViewModel 생성

---
+++ MVVM/MVVM/View/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  MVVM
+//
+//  Created by 서현웅 on 2023/07/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var stateImageView: UIImageView!
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var pwTextField: UITextField!
+    @IBOutlet weak var confirmPwTextField: UITextField!
+    @IBOutlet weak var signInButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func signInButton(_ sender: UIButton) {
+        
+    }
+    
+
+}
+
---
+++ MVVM/MVVM/View/ViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ViewModel.swift
+//  MVVM
+//
+//  Created by 서현웅 on 2023/07/12.
+//
+
+import Foundation
+
+class ViewModel {
+    
+}

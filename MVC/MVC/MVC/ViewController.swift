28b9c78831fc991a1d5ec51f37e7497ef2492a17
woong <workplayhard1@naver.com>
2023-07-12 00:28:29 +0900

    feat: UI 초기구현

---
+++ MVC/MVC/MVC/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MVC
+//
+//  Created by 서현웅 on 2023/07/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var stateImageView: UIImageView!
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var pwTextField: UITextField!
+    @IBOutlet weak var confirmPwTextField: UITextField!
+    @IBOutlet weak var signInButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func signInAction(_ sender: Any) {
+        
+    }
+    
+}
+

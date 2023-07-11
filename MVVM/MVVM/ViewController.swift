2d6e7ffaa8ae96d8718ffce447be7fcaa30fe4d8
woong <workplayhard1@naver.com>
2023-07-12 03:12:04 +0900

    feat: MVVM 프로젝트 생성

---
+++ MVVM/MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM
+//
+//  Created by 서현웅 on 2023/07/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

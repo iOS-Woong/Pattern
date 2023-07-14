6a59620529f6761570b638bde87d65d52d7fc146
woong <workplayhard1@naver.com>
2023-07-14 15:09:22 +0900

    feat: IBOutlet

---
+++ MVVM+Rx/MVVM+Rx/Views/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MVVM+Rx
+//
+//  Created by 서현웅 on 2023/07/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var stateImageView: UIImageView!
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var pwTextField: UITextField!
+    @IBOutlet weak var confirmPwTextField: UITextField!
+    @IBOutlet weak var signInButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    
+
+}
+
---
+++ MVVM+Rx/MVVM+Rx/Views/ViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ViewModel.swift
+//  MVVM+Rx
+//
+//  Created by 서현웅 on 2023/07/14.
+//
+
+import Foundation
+
+class ViewModel {
+    
+}
